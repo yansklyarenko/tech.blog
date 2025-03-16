746855544329df69d95de8d9931ca2fc8348c601
Yan Sklyarenko <yansklyarenko@gmail.com>
2025-03-16 18:11:20 +0100

    Use newer .NET version

--- build.ps1
+++ build.ps1
@@ -18,7 +18,7 @@ If (!(Test-Path $ToolsDir))
 
 if (!(Test-Path $ToolsProj))
 {
-    $projectFileContents = '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp2.0</TargetFramework></PropertyGroup></Project>'
+    $projectFileContents = '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>net8.0</TargetFramework></PropertyGroup></Project>'
     Out-File -InputObject $projectFileContents -FilePath $ToolsProj
 }
 
